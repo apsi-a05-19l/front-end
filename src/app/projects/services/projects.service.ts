bc2ecf9b0b5f2c167c918f53fe0d28eddfde7af7
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2019-04-08 12:00:37 +0200

    projects list + bootstrap, font awesome injection

---
+++ src/app/projects/services/projects.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {ProjectModel} from '../models/ProjectModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+
+  constructor() { }
+
+  fetchProjectsList(): Promise<ProjectModel[]> {
+    return Promise.resolve([
+      {
+        id: 1,
+        name: 'DripSens',
+      },
+      {
+        id: 2,
+        name: 'Prąd do serca',
+      },
+      {
+        id: 3,
+        name: 'Gods Eye',
+      }
+    ]);
+  }
+}
