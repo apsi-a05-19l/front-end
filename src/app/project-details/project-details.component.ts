631dfe7d6bfb0d1290bda91cee98dfb731a78e7b
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2019-04-22 14:50:44 +0200

    Form in modal to add report

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {ProjectDetailsService} from './services/project-details.service';
 import {ProjectDetailsModel} from './models/project-details.model';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ReportModel} from './models/report.model';
 
 @Component({
   selector: 'app-project-details',
@@ -12,20 +13,31 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 export class ProjectDetailsComponent implements OnInit {
   project: ProjectDetailsModel;
   projectId: number;
+  reportToEdit: ReportModel;
+
   constructor(private route: ActivatedRoute, private service: ProjectDetailsService, private modalService: NgbModal) { }
 
   ngOnInit() {
+    this.reportToEdit = new ReportModel();
     this.fetchProjectInfo();
   }
 
   fetchProjectInfo() {
     this.projectId = Number(this.route.snapshot.params.id);
-    this.service.fetchProject(this.projectId).then((project: ProjectDetailsModel) => this.project = project);
+    this.service.fetchProject(this.projectId)
+      .then((project: ProjectDetailsModel) => {
+        this.project = project;
+        this.reportToEdit.author = project.currentLeader;
+      });
   }
 
   addProjectReport(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
-    console.log('add project report');
+  }
+
+  saveReport(content) {
+    this.service.saveReport(this.reportToEdit).then((report) => console.log(report));
+    content.close();
   }
 
 }
--- src/app/project-details/services/project-details.service.ts
+++ src/app/project-details/services/project-details.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ProjectDetailsModel} from '../models/project-details.model';
 import {mockProjectDetails} from '../mock-data/mock-project-details';
+import {ReportModel} from '../models/report.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectDetailsService {
 
-  constructor() { }
+  constructor() {
+  }
 
   fetchProject(projectId: number): Promise<ProjectDetailsModel> {
     const projectToReturn: ProjectDetailsModel = mockProjectDetails.filter(project => project.id === projectId)[0];
     return Promise.resolve(projectToReturn);
+  }
+
+  saveReport(reportToSave: ReportModel): Promise<ReportModel> {
+    return Promise.resolve(reportToSave);
   }
 }
